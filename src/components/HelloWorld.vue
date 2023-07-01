fc4bd1e5b5844d45713d196493d4ebfed9f1af83
Shirley R <srm.shirley@gmail.com>
2023-06-30 19:18:24 -0500

    Cleaned home template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,12 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
@@ -24,16 +19,14 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+  color:#000000;
 }
 
 h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+
 
 @media (min-width: 1024px) {
   .greetings h1,
